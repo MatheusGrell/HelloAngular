ce08ff74e80a1f8f31902c38788a1c73265b2ec6
Matheus Grell <matheus.grell@iteris.com.br>
2019-04-24 12:28:13 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagLayoutComponent } from './pag-layout/pag-layout.component';
+import { BoloListComponent } from './bolo/bolo-list/bolo-list.component';
+import { PaisesListComponent } from './paises/paises-list/paises-list.component';
+import { TemplateDriveComponent } from './cadastros/template-drive/template-drive.component';
+import { ModelDriveComponent } from './cadastros/model-drive/model-drive.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PagLayoutComponent,
+  children: [
+    { path: 'bolo', component: BoloListComponent },
+    { path: 'paises', component: PaisesListComponent },
+    { path: 'modelform', component: ModelDriveComponent },
+    { path: 'adm', loadChildren: './admin/admin.module#AdminModule' },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoloListComponent } from './bolo/bolo-list/bolo-list.component';
+import { PaisesModule } from './paises/paises.module';
+import { PagLayoutComponent } from './pag-layout/pag-layout.component';
+import { CadastrosModule } from './cadastros/cadastros.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoloListComponent,
+    PagLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PaisesModule,
+    CadastrosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
